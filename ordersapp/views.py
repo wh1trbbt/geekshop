e9ac6d9c2f62c510ff31ba517f7d853f76533c7a
Alexander <wh1trbbt@gmail.com>
2022-02-06 18:19:49 +0300

    lesson_12. Product's count as signal

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,4 +1,6 @@
 from django.db import transaction
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
 from django.forms import inlineformset_factory
 from django.shortcuts import HttpResponseRedirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -6,6 +8,7 @@ from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.views.generic.detail import DetailView
 
 from basketapp.models import Basket
+from mainapp.models import Product
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
 
@@ -113,3 +116,22 @@ def order_forming_complete(request, pk):
     order.save()
 
     return HttpResponseRedirect(reverse("ordersapp:orders_list"))
+
+
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_save(instance, sender, **kwargs):
+    if instance.pk:
+        """If user change quantity in order or basket"""
+        instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+    else:
+        """If user create order or basket"""
+        instance.product.quantity -= instance.quantity
+    instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(instance, **kwargs):
+    instance.product.quantity += instance.quantity
+    instance.product.save()
