80384906ad9e28a6da554f53f6be0e04922028be
Alexander <wh1trbbt@gmail.com>
2022-02-13 18:33:59 +0300

    lesson_14. debug-tool-bar, fixed

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -195,16 +195,20 @@ SOCIAL_AUTH_GITHUB_SECRET = github_auth["client_secret"]
 
 # Django Debug Toolbar --->
 if DEBUG:
-    INSTALLED_APPS.extend([
-        "debug_toolbar",
-        "template_profiler_panel",
-    ])
+    INSTALLED_APPS.extend(
+        [
+            "debug_toolbar",
+            "template_profiler_panel",
+        ]
+    )
 
 
 if DEBUG:
-    MIDDLEWARE.extend([
-        "debug_toolbar.middleware.DebugToolbarMiddleware",
-    ])
+    MIDDLEWARE.extend(
+        [
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        ]
+    )
 
 # Debgu tool bar settings
 if DEBUG:
